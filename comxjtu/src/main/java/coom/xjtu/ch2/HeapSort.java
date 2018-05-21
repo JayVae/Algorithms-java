9df31cc6d6ed0353d37d372574c3dcb3b0014c04
JayVae <919433524@qq.com>
2018-05-21 16:20:37 +0800

    '堆和堆排序'

---
+++ comxjtu/src/main/java/coom/xjtu/ch2/HeapSort.java
@@ -0,0 +1,53 @@
+package coom.xjtu.ch2;
+
+/**
+ * @Author: Jay
+ * @Date: Created in 15:04 2018/5/21
+ * @Modified By:
+ */
+
+/**
+ * 堆排序：
+ *  1.构造堆；
+ *  2.堆下沉
+ */
+public class HeapSort {
+
+    public static void sort(Comparable[] pq) {
+        int n = pq.length;
+        for (int k = n/2; k >= 1; k--)
+            sink(pq, k, n);
+        while (n > 1) {
+            exch(pq, 1, n--);
+            sink(pq, 1, n);
+        }
+    }
+
+    private static boolean less(Comparable[] pq, int i, int j) {
+        return pq[i-1].compareTo(pq[j-1]) < 0;
+    }
+
+    private static void exch(Object[] pq, int i, int j) {
+        Object swap = pq[i-1];
+        pq[i-1] = pq[j-1];
+        pq[j-1] = swap;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+    }
+
+    private static void sink(Comparable[] pq, int k, int n) {
+        while (2*k <= n) {
+            int j = 2*k;
+            if (j < n && less(pq, j, j+1)) j++;
+            if (!less(pq, k, j)) break;
+            exch(pq, k, j);
+            k = j;
+        }
+    }
+
+}
---
+++ comxjtu/src/main/java/coom/xjtu/ch2/MaxPQ.java
@@ -0,0 +1,72 @@
+package coom.xjtu.ch2;
+
+/**
+ * @Author: Jay
+ * @Date: Created in 14:25 2018/5/21
+ * @Modified By:
+ */
+
+/**
+ * 堆实现优先队列
+ * 改进之处：
+ *  1.多叉堆
+ *  2.调整数组大小
+ *  3.索引优先队列
+ * @param <Key>
+ */
+public class MaxPQ<Key extends Comparable<Key>> {
+    private Key[] pq;
+    private int N = 0;
+
+    public MaxPQ(int maxN) {
+        pq = (Key[]) new Comparable[maxN+1];
+    }
+
+    public boolean isEmpty(){
+        return N==0;
+    }
+
+    public int size(){
+        return N;
+    }
+
+    public void insert(Key v){
+        pq[++N] = v;
+        swim(N);
+    }
+
+    public Key delMax(){
+        Key max = pq[1];
+        exch(1,N--);
+        pq[N+1]=null;
+        sink(1);
+        return max;
+    }
+
+    private boolean less(int i,int j){
+        return pq[i].compareTo(pq[j])<0;
+    }
+
+    private void exch(int i, int j){
+        Key tmp = pq[i];
+        pq[i] = pq[j];
+        pq[j] = tmp;
+    }
+
+    public void swim(int k){
+        while(k>1 && less(k/2,k)){
+            exch(k/2,k);
+            k=k/2;
+        }
+    }
+
+    public void sink(int k){
+        while(2*k < N ){
+            int j = 2*k;
+            if (j<N && less(j,j+1)) j++;
+            if (!less(k,j)) break;
+            exch(k,j);
+            k = j;
+        }
+    }
+}
