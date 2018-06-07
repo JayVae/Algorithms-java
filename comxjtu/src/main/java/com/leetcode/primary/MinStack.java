dc046b3560452326b0aa1cb7efbbb5d2018ed34a
JayVae <919433524@qq.com>
2018-06-07 15:54:05 +0800

    ''

--- comxjtu/src/main/java/com/leetcode/primary/MinStack.java
+++ comxjtu/src/main/java/com/leetcode/primary/MinStack.java
@@ -7,23 +7,52 @@ package com.leetcode.primary;
  */
 public class MinStack {
     /** initialize your data structure here. */
+    Node first;
+    Node min;
     public MinStack() {
 
     }
 
     public void push(int x) {
+        if (first==null){
+            first = new Node(x);
+            min = new Node(x);
+        }else {
+            Node oldfirst = first;
+            first = new Node(x);
+            first.next = oldfirst;
+            if (x<min.val){
+                Node oldmin = min;
+                min = new Node(x);
+                min.next = oldmin;
+            }else {
+                Node oldmin = min;
+                min = new Node(oldmin.val);
+                min.next = oldmin;
+            }
+        }
 
     }
 
     public void pop() {
-
+        first = first.next;
+        min = min.next;
     }
 
     public int top() {
-        return 0;
+        return first.val;
     }
 
     public int getMin() {
-        return 0;
+        return min.val;
+    }
+
+    public class Node{
+        private int val;
+        private Node next;
+
+        public Node(int val) {
+            this.val = val;
+        }
     }
 }
