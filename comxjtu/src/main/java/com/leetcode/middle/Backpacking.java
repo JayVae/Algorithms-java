e285002d288de60c59546e8fc2dccd045aa12857
JayVae <919433524@qq.com>
2018-06-15 16:53:50 +0800

    ''

--- comxjtu/src/main/java/com/leetcode/middle/Backpacking.java
+++ comxjtu/src/main/java/com/leetcode/middle/Backpacking.java
@@ -133,6 +133,34 @@ public class Backpacking {
         }
     }
 
+    /**
+     * 单词搜索：给定一个二维网格和一个单词，找出该单词是否存在于网格中。
+     * @param board
+     * @param word
+     * @return
+     */
+    public boolean exist(char[][] board, String word) {
+        char[] w = word.toCharArray();
+        for (int y=0; y<board.length; y++) {
+            for (int x=0; x<board[y].length; x++) {
+                if (exist(board, y, x, w, 0)) return true;
+            }
+        }
+        return false;
+    }
+    private boolean exist(char[][] board, int y, int x, char[] word, int i) {
+        if (i == word.length) return true;
+        if (y<0 || x<0 || y == board.length || x == board[y].length) return false;
+        if (board[y][x] != word[i]) return false;
+        board[y][x] ^= 256;
+        boolean exist = exist(board, y, x+1, word, i+1)
+                || exist(board, y, x-1, word, i+1)
+                || exist(board, y+1, x, word, i+1)
+                || exist(board, y-1, x, word, i+1);
+        board[y][x] ^= 256;
+        return exist;
+    }
+
     public static void main(String[] args){
         int i=0;
         String tmp = Integer.toBinaryString(i);
