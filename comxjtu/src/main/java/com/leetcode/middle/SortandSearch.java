92f09baf37e56bfa558d600226572f48690a6514
JayVae <919433524@qq.com>
2018-06-19 11:00:08 +0800

    ''

--- comxjtu/src/main/java/com/leetcode/middle/SortandSearch.java
+++ comxjtu/src/main/java/com/leetcode/middle/SortandSearch.java
@@ -1,10 +1,106 @@
 package com.leetcode.middle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author: Jay
  * @Date: Created in 20:56 2018/6/16
  * @Modified By:
  */
 public class SortandSearch {
-    //今天是端午啊，下雨，心情平静。
+    //今天是端午啊，下雨，心情平静,什么都没做，还一直没网~~~
+
+    /**
+     * 分类颜色：
+     * 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+     * 方法二是遍历一次，常数空间。
+     * @param nums
+     */
+    public void sortColors(int[] nums) {
+        if (nums.length<2) return;
+        int[] map = new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            switch (nums[i]){
+                case 0:
+                    map[0]++;
+                    break;
+                case 1:
+                    map[1]++;
+                    break;
+                case 2:
+                    map[2]++;
+                    break;
+                default:
+                    break;
+            }
+        }
+        for (int i = 0; i < map[0]; i++) {
+            nums[i]=0;
+        }
+        for (int i = map[0]; i < map[1] + map[0]; i++) {
+            nums[i]=1;
+        }
+        for (int i = map[0]+map[1]; i < nums.length; i++) {
+            nums[i]=2;
+        }
+    }
+    public void sortColors1(int[] nums) {
+        int i = -1,j = -1,k = -1;
+        for(int m = 0; m < nums.length; m++){
+            if(nums[m] == 0){
+                nums[++k] = 2;
+                nums[++j] = 1;
+                nums[++i] = 0;
+            }else if(nums[m] == 1){
+                nums[++k] = 2;
+                nums[++j] = 1;
+            }else{
+                nums[++k] = 2;
+            }
+        }
+    }
+
+    /**
+     * Top K Frequent Elements
+     给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+     * @param nums
+     * @param k
+     * @return
+     */
+    public List<Integer> topKFrequent(int[] nums, int k) {
+
+        List<Integer> res = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n > max)
+                max = n;
+            if (n < min)
+                min = n;
+        }
+        int[] bucket = new int[max - min + 1];
+        for (int n : nums)
+            bucket[n - min]++;
+
+        List<Integer>[] array = new ArrayList[nums.length + 1];
+        for (int i = 0; i < bucket.length; i++) {
+            if (array[bucket[i]] == null) {
+                array[bucket[i]] = new ArrayList<>();
+            }
+            array[bucket[i]].add(i + min);
+        }
+        for (int i = array.length - 1; i > 0; i--) {
+            if (array[i] != null) {
+                for (Integer n : array[i]) {
+                    res.add(n);
+                    if (res.size() == k)
+                        return res;
+                }
+            }
+        }
+
+        return res;
+
+    }
 }
--- comxjtu/src/main/java/com/leetcode/middle/TreeandGraph.java
+++ comxjtu/src/main/java/com/leetcode/middle/TreeandGraph.java
@@ -72,7 +72,37 @@ public class TreeandGraph {
         intranverse(root.right);
     }
 
+    /**
+     * 岛屿的个数：
+     * 给定一个由 '1'（陆地）和 '0'（水）组成的的二维网格，计算岛屿的数量。一个岛被水包围，并且它是通过水平方向或垂直方向上相邻的陆地连接而成的。你可以假设网格的四个边均被水包围。
+     * @param grid
+     * @return
+     */
+    private int n;
+    private int m;
 
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        n = grid.length;
+        if (n == 0) return 0;
+        m = grid[0].length;
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++)
+                if (grid[i][j] == '1') {
+                    DFSMarking(grid, i, j);
+                    ++count;
+                }
+        }
+        return count;
+    }
+    private void DFSMarking(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
+        grid[i][j] = '0';
+        DFSMarking(grid, i + 1, j);
+        DFSMarking(grid, i - 1, j);
+        DFSMarking(grid, i, j + 1);
+        DFSMarking(grid, i, j - 1);
+    }
     public class TreeNode {
         int val;
         TreeNode left;
