9ac4302c29ebde28a9dfd9a28da07132ebaeeef3
JayVae <919433524@qq.com>
2018-06-21 21:38:29 +0800

    ''

--- comxjtu/src/main/java/com/leetcode/middle/SortandSearch.java
+++ comxjtu/src/main/java/com/leetcode/middle/SortandSearch.java
@@ -404,12 +404,44 @@ public class SortandSearch {
      * @return
      */
     public int search(int[] nums, int target) {
-
-    }
-    private int binaryMiddle(int[] nums){
-        int lo = 0, hi = nums.length-1;
-        while (lo<=hi){
-            int mi
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (target == nums[mid])
+                return mid;
+            if (nums[mid] < nums[lo]) {
+                // 6,7,0,1,2,3,4,5
+                if (target < nums[mid] || target >= nums[lo])
+                    hi = mid - 1;
+                else
+                    lo = mid + 1;
+            } else {
+                // 2,3,4,5,6,7,0,1
+                if (target > nums[mid] || target < nums[lo])
+                    lo = mid + 1;
+                else
+                    hi = mid - 1;
+            }
         }
+        return -1;
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length<1) return false;
+        int m = matrix.length, n = matrix[0].length;
+        if (n<1) return false;
+        int i = m-1;
+        int j = 0;
+        while (j<n && i>=0){
+                if (matrix[i][j]>target) {
+                    i--;
+                }else if(matrix[i][j]<target){
+//                    if (n==1) return false;
+                    j++;
+                }else {
+                    return true;
+                }
+        }
+        return false;
     }
 }
