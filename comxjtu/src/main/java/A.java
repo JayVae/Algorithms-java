0ecfe251008035d9e93d6d7ed7d85c4e4494bf99
JayVae <919433524@qq.com>
2018-08-19 21:50:39 +0800

    '回溯法'

---
+++ comxjtu/src/main/java/A.java
@@ -0,0 +1,50 @@
+/**
+ * @Author: Jay
+ * @Date: Created in 19:45 2018/8/11
+ * @Modified By:
+ */
+
+import java.util.Scanner;
+
+public class A {
+    int n, k;
+    int a[] = new int[1000005];
+    int b[] = new int[100005];
+    int dp[] = new int[100005];
+    int dp1[] = new int[100005];
+    Scanner scanner;
+
+    public void solve() {
+        scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        k = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            b[i] = scanner.nextInt();
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = a[i] + dp[i + 1];
+            dp1[i] = dp1[i + 1];
+            if (b[i] == 1) {
+                dp1[i] += a[i];
+            }
+        }
+        int sum = 0, ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (b[i] == 1) {
+                sum += a[i];
+            } else {
+                int j = Math.min(i + k - 1, n);
+                ans = Math.max(ans, sum + dp[i] - dp[j + 1] + dp1[j + 1]);
+            }
+        }
+        ans = Math.max(ans, sum);
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        new A().solve();
+    }
+}
--- comxjtu/src/main/java/com/leetcode/middle/Backpacking.java
+++ comxjtu/src/main/java/com/leetcode/middle/Backpacking.java
@@ -38,7 +38,9 @@ public class Backpacking {
     }
 
     /**
-     * 全排列
+     * lc46
+     * 全排列:
+     * 给定一个没有重复数字的序列，返回其所有可能的全排列。
      * @param nums
      * @return
      */
@@ -57,6 +59,88 @@ public class Backpacking {
                 if(tempList.contains(nums[i])) continue; // element already exists, skip
                 tempList.add(nums[i]);
                 backtrack(list, tempList, nums);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
+    /**
+     * lc47
+     * 给定一个可包含重复数字的序列，返回所有不重复的全排列。
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, new boolean[nums.length]);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, boolean [] used){
+        if(tempList.size() == nums.length){
+            list.add(new ArrayList<>(tempList));
+        } else{
+            for(int i = 0; i < nums.length; i++){
+                if(used[i] || i > 0 && nums[i] == nums[i-1] && !used[i - 1]) continue;
+                used[i] = true;
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, used);
+                used[i] = false;
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
+    /**
+     * lc39
+     * 组合求和(可以用重复数字)
+     * @param nums
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, target, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+        if(remain < 0) return;
+        else if(remain == 0) list.add(new ArrayList<>(tempList));
+        else{
+            for(int i = start; i < nums.length; i++){
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
+    /**
+     * lc40
+     * 组合求和：不可以用重复数字
+     * @param nums
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> combinationSum2(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrackSum2(list, new ArrayList<>(), nums, target, 0);
+        return list;
+
+    }
+
+    private void backtrackSum2(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+        if(remain < 0) return;
+        else if(remain == 0) list.add(new ArrayList<>(tempList));
+        else{
+            for(int i = start; i < nums.length; i++){
+                if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
+                tempList.add(nums[i]);
+                backtrackSum2(list, tempList, nums, remain - nums[i], i + 1);
                 tempList.remove(tempList.size() - 1);
             }
         }
@@ -92,7 +176,10 @@ public class Backpacking {
     }
 
     /**
+     * lc78
      * 子集：给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+     * 方法一使用了类似的穷举法；
+     * 方法二使用了回溯法的递归实现。
      * @param nums
      * @return
      */
@@ -111,8 +198,24 @@ public class Backpacking {
         }
         return result;
     }
+    public List<List<Integer>> subsets2(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack1(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
+
+    private void backtrack1(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+        list.add(new ArrayList<>(tempList));
+        for(int i = start; i < nums.length; i++){
+            tempList.add(nums[i]);
+            backtrack1(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
 
     /**
+     * lc90
      * 子集2：给定一个可能包含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
      * @param nums
      * @return
