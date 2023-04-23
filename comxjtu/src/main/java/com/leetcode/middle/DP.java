77cd4de2efb922f8d8e21474ea48d810bde210dc
JayVae <919433524@qq.com>
2023-04-23 22:07:54 +0800

    leetcode exercise

--- comxjtu/src/main/java/com/leetcode/middle/DP.java
+++ comxjtu/src/main/java/com/leetcode/middle/DP.java
@@ -211,5 +211,24 @@ public class DP {
         }
         return ans;
     }
+//    1105. 填充书架
+    public int minHeightShelves(int[][] books, int shelfWidth) {
+        int n = books.length;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, 1000000);
+        dp[0] = 0;
+        for (int i = 0; i < n; ++i) {
+            int maxHeight = 0, curWidth = 0;
+            for (int j = i; j >= 0; --j) {
+                curWidth += books[j][0];
+                if (curWidth > shelfWidth) {
+                    break;
+                }
+                maxHeight = Math.max(maxHeight, books[j][1]);
+                dp[i + 1] = Math.min(dp[i + 1], dp[j] + maxHeight);
+            }
+        }
+        return dp[n];
+    }
 }
 
