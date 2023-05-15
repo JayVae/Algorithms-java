c34e83375fa57b722ac1d762d486aae8d91cf4ae
JayVae <919433524@qq.com>
2023-05-15 09:44:18 +0800

    leetcode exercise

---
+++ comxjtu/src/main/java/com/leetcode/everyday/Test.java
@@ -0,0 +1,17 @@
+package com.leetcode.everyday;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+
+    // find all images without alternate text
+// and give them a red border
+    void process () {
+        // find all images without alternate text
+// and give them a red border
+
+
+    }
+
+}
